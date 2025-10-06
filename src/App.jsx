8bb12eedfc3c865bd4f22d8268ad78844a7e5a74
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-10-06 17:13:37 +0200

    incartato nel form input e button; creata una variabile di stato con il valore dell'array di partenza(tasks) e il valore che deve aggiornarsi (setTasks) per aggiornare la UI. Usato map per iterare all'interno dell'array

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import { useState } from 'react'
 
 function App() {
   const [list, setlist] = useState('')
-
+  const [tasks, setTasks] = useState(['studiare react', 'studiare JavaScript', 'studiare CSS'])
   return (
     <>
       <h1>To Do List</h1>
       <form>
-
         <input type="text" value={list} onChange={(e) => setlist(e.target.value)} placeholder='scrivi la tua lista' />
-        <button>Add task</button>
-        <ul>
-          <li></li>
-        </ul>
-
+        <button type='submit'>Add task</button>
       </form>
+      <ul>
+        {tasks.map((task, i) => {
+          return (
+            <li key={i}>{task}</li>
+
+          )
+        })}
+      </ul>
+
     </>
   )
 }
